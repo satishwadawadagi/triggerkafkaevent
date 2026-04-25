c211b2c7d62edaece2ef0b0b0cec2447de5a77da
satishwadawadagi <satishbros@gmail.com>
2026-04-25 22:45:03 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from multi-tenant app');
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
+app.listen(3000, () => console.log('App running on port 3000'));
